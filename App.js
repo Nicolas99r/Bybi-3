89baedd41865acb934e3c0c2513503324d36c69f
oscarloaiza666 <est.oscar.loaiza@unimilitar.edu.co>
2021-05-23 18:58:49 -0500

    Finciona la base de datos (parcialmente)
    Funciona crearUsuario
    Aún falta createBike

--- App.js
+++ App.js
@@ -1,34 +1,32 @@
 //Componentes de react y react navigation
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 //Importar pantallas
-import Createuserscreen from './screens/Createuserscreen/Createuserscreen';
-import Createbikescreen from './screens/Createbikescreen/Createbikescreen'
-import Loginscreen from './screens/Loginscreen/Loginscreen'
-import LoginInst from './screens/LoginInst/LoginInst'
+import Createuserscreen from "./screens/Createuserscreen/Createuserscreen";
+import Createbikescreen from "./screens/Createbikescreen/Createbikescreen";
+import Loginscreen from "./screens/Loginscreen/Loginscreen";
+import LoginInst from "./screens/LoginInst/LoginInst";
 //import EleccionUser from './screens/EleccionUser/EleccionUser';
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 //Funcion que almacena las pantallas
-function MyStack(){
-  return(
+function MyStack() {
+  return (
     <Stack.Navigator>
-    
-      <Stack.Screen name="Loginscreen" component={Loginscreen}/>
-      <Stack.Screen name="LoginInst" component={LoginInst}/>
-      <Stack.Screen name="Createuserscreen" component={Createuserscreen}/>
-      <Stack.Screen name="Createbikescreen" component={Createbikescreen}/>
-      
+      <Stack.Screen name="Loginscreen" component={Loginscreen} />
+      <Stack.Screen name="LoginInst" component={LoginInst} />
+      <Stack.Screen name="Createuserscreen" component={Createuserscreen} />
+      <Stack.Screen name="Createbikescreen" component={Createbikescreen} />
     </Stack.Navigator>
-  )
+  );
 }
 
 //App principal
 export default function App() {
   return (
     <NavigationContainer>
-      <MyStack/>
+      <MyStack />
     </NavigationContainer>
   );
 }
--- database/firebase.js
+++ database/firebase.js
@@ -1,24 +1,56 @@
-import firebase from 'firebase'
-
-import 'firebase/firestore'
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyA5CmAWmSjMLjnxigF85jjgqhei4UKxuPQ",
-    authDomain: "pruebita-1c4c2.firebaseapp.com",
-    databaseURL: "https://pruebita-1c4c2-default-rtdb.firebaseio.com",
-    projectId: "pruebita-1c4c2",
-    storageBucket: "pruebita-1c4c2.appspot.com",
-    messagingSenderId: "126371026855",
-    appId: "1:126371026855:web:47b947df98cbb785700882",
-    measurementId: "G-3770J9C0B2"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  //firebase.analytics();
+  apiKey: "AIzaSyA5CmAWmSjMLjnxigF85jjgqhei4UKxuPQ",
+  authDomain: "pruebita-1c4c2.firebaseapp.com",
+  databaseURL: "https://pruebita-1c4c2-default-rtdb.firebaseio.com",
+  projectId: "pruebita-1c4c2",
+  storageBucket: "pruebita-1c4c2.appspot.com",
+  messagingSenderId: "126371026855",
+  appId: "1:126371026855:web:47b947df98cbb785700882",
+  measurementId: "G-3770J9C0B2",
+};
 
-  const db = firebase.firestore()
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
 
+/*const db = firebase.firestore()
   export default {
       firebase,
       db
-  }
+  }*/
+
+function writeUserData(user) {
+  firebase
+    .database()
+    .ref("users/" + user.uid)
+    .set(user)
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
+firebase.createUser = async (
+  setNombre,
+  setCorreoInstitucional,
+  setCodigoInstitucional,
+  setPassword
+) => {
+  await firebase
+    .auth()
+    .createUserWithEmailAndPassword(setCorreoInstitucional, setPassword)
+    .then(function (data) {
+      const user = {
+        nombre: setNombre,
+        codigoInstitucional: setCodigoInstitucional,
+        correoInstitucional: setCorreoInstitucional,
+        uid: data.user.uid,
+      };
+      console.log(data.user.uid);
+      writeUserData(user);
+    });
+};
+
+export default firebase;
